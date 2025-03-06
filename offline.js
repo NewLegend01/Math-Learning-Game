ce413be66c15e73f7234880e1de5707de43747f8
NewLegend01 <newaera01@gmail.com>
2025-03-07 01:27:58 +0700

    update

--- offline.js
+++ offline.js
@@ -1,11 +1,13 @@
 ﻿{
-	"version": 1739202971,
+	"version": 1741285592,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/bg-sheet0.png",
+		"media/c418.ogg",
+		"media/c418.m4a",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
